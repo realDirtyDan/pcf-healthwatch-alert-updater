a24708cf8f07a4c98c34b4f8a1076cdd839524ff
David Mathis <dbmathis@gmail.com>
2019-03-31 10:08:18 -0400

    added get-config and dropped threshold reqs

--- hw-alert-update.sh
+++ hw-alert-update.sh
@@ -10,6 +10,8 @@ die() {
 
 # Initialize all the option variables.
 # This ensures we are not contaminated by variables from the environment.
+config=
+dry=
 api=
 critical=0
 warning=0
@@ -17,10 +19,16 @@ type=0
 query=
 verbose=0
 
+typecheck=false
+warningcheck=false
+criticalcheck=false
+
+# Define usage
 function usage {
     cat <<EOM
 Usage: $(basename "$0") [OPTION]...
 
+  --get-config          show current alert configuration
   -d|--dry              show results without making changes to alerts
   -a|--api      STRING  healthwatch-api.SYSTEM-DOMAIN/v1/alert-configurations
   -c|--critical NUM     critical threshold
@@ -28,19 +36,28 @@ Usage: $(basename "$0") [OPTION]...
   -t|--type     STRING  threeshold type
   -q|--query    REGEX   alert query search string  
   -h|--help             show this help            
-EOM
 
+Examples:
+
+  $ ./hw-alert-update.sh -a healthwatch-api.SYSTEM-DOMAIN/v1/alert-configurations --get-config
+  $ ./hw-alert-update.sh -a healthwatch-api.SYSTEM-DOMAIN/v1/alert-configurations -t UPPER -w 100 -c 200 -q 'latency.uaa' --dry
+  $ ./hw-alert-update.sh -a healthwatch-api.SYSTEM-DOMAIN/v1/alert-configurations -t UPPER -w 100 -c 200 -q 'latency.uaa'
+EOM
     exit 2
 }
 
+# Process options
 while :; do
     case $1 in
         -h|-\?|--help)
-            usage  # Display a usage synopsis.
+            usage           # Display a usage synopsis.
             exit
             ;;
+        --get-config)
+            config=true     # config option true
+            ;;
         -d|--dry)
-            dry=true        # dry run - not update to alerts
+            dry=true        # dry run option true
             ;;
         -a|--api)           # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
@@ -59,58 +76,44 @@ while :; do
         -c|--critical)      # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 critical=$2
+                criticalcheck=true
                 shift
-            else
-                die 'ERROR: "--critical" requires a non-empty option argument.'
             fi
             ;;
         --critical=?*)
             critical=${1#*=}# Delete everything up to "=" and assign the remainder.
-            ;;
-        --critical=)        # Handle the case of an empty --critical=
-            die 'ERROR: "--critical" requires a non-empty option argument.'
+            criticalcheck=true
             ;;
         -w|--warning)       # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 warning=$2
+                warningcheck=true
                 shift
-            else
-                die 'ERROR: "--warning" requires a non-empty option argument.'
             fi
             ;;
         --warning=?*)
             warning=${1#*=} # Delete everything up to "=" and assign the remainder.
-            ;;
-        --warning=)         # Handle the case of an empty --warning=
-            die 'ERROR: "--warning" requires a non-empty option argument.'
+            warningcheck=true
             ;;
         -t|--type)          # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 type=$2
+                typecheck=true
                 shift
-            else
-                die 'ERROR: "--type" requires a non-empty option argument.'
             fi
             ;;
         --type=?*)
             type=${1#*=}    # Delete everything up to "=" and assign the remainder.
-            ;;
-        --type=)            # Handle the case of an empty --type=
-            die 'ERROR: "--type" requires a non-empty option argument.'
+            typecheck=true
             ;;
         -q|--query)         # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 query=$2
                 shift
-            else
-                die 'ERROR: "--query" requires a non-empty option argument.'
             fi 
             ;;
         --query=?*)
             query=${1#*=}   # Delete everything up to "=" and assign the remainder.
-            ;;
-        --query=)           # Handle the case of an empty --query=
-            die 'ERROR: "--query" requires a non-empty option argument.'
             ;;
         -v|--verbose)
             verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
@@ -129,19 +132,45 @@ while :; do
     shift
 done
 
+# Set token env
 export token=$(uaac context | grep access_token | awk '{print $2}')
 
-# 
-if [ "$dry" == true ]; then
+function join { local IFS="$1"; shift; echo "$*"; }
+
+# Setup update for jq
+select=".[] | select(.query|test(\"${query}\")) |"
+    
+thresholds=()
+
+if [ "$criticalcheck" == true ]; then
+    thresholds+=(".threshold.critical=${critical}")
+fi
+if [ "$typecheck" == true ]; then
+    thresholds+=(".threshold.type=\"${type}\"") 
+fi 
+if [ "$warningcheck" == true ]; then
+    thresholds+=(".threshold.warning=${warning}") 
+fi
+update=$(join '|' ${thresholds[@]})
+
+# Display alert configurations
+if [ "$config" == true ] && [ "$api" ]; then
+    curl -sG "$api" -H "Authorization: Bearer ${token}" | jq
+# Perform a dry run
+elif [ "$dry" == true ] && [ "$api" ]; then
+    # Output entities that are targeted for update
     echo 'BEFORE:'
     curl -sG "$api" -H "Authorization: Bearer ${token}" | 
     jq ".[] | select(.query|test(\"${query}\"))"
     echo ''
+    # Output entities after update
     echo 'AFTER:'
-    curl -sG "$api" -H "Authorization: Bearer ${token}" |
-    jq ".[] | select(.query|test(\"${query}\")) | .threshold.critical = ${critical} | .threshold.type = \"${type}\" | .threshold.warning = ${warning}"   
+    curl -sG "$api" -H "Authorization: Bearer ${token}" | jq "${select} ${update}"   
 elif [ "$api" ] && [ "$critical" ] && [ "$warning" ] && [ "$type" ] && [ "$query" ]; then
-    curl -sG "$api" -H "Authorization: Bearer ${token}" | 
-    jq ".[] | select(.query|test(\"${query}\")) | .threshold.critical = ${critical} | .threshold.type = \"${type}\" | .threshold.warning = ${warning}" | 
+    # Update entities
+    echo 'AFTER:'
+    curl -sG "$api" -H "Authorization: Bearer ${token}" | jq "${select} ${update}" |
     curl -d @- -H "Authorization: Bearer ${token}" -H "Accept: application/json" -H "Content-Type: application/json" "$api"
+else
+    usage
 fi
