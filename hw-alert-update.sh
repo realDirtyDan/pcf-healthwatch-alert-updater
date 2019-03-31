94d441aef0eeb01148691f8458aadbd6cf191b31
David Mathis <dbmathis@gmail.com>
2019-03-30 20:04:06 -0400

    Added options

--- hw-alert-update.sh
+++ hw-alert-update.sh
@@ -10,15 +10,18 @@ die() {
 # This ensures we are not contaminated by variables from the environment.
 api=
 critical=0
+warning=0
+type=0
+query=
 verbose=0
 
 while :; do
     case $1 in
         -h|-\?|--help)
-            echo 'No help'    # Display a usage synopsis.
+            echo 'No help'  # Display a usage synopsis.
             exit
             ;;
-        -a|--api)       # Takes an option argument; ensure it has been specified.
+        -a|--api)           # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 api=$2
                 shift
@@ -27,36 +30,78 @@ while :; do
             fi
             ;;
         --api=?*)
-            api=${1#*=} # Delete everything up to "=" and assign the remainder.
+            api=${1#*=}     # Delete everything up to "=" and assign the remainder.
             ;;
-        --api=)         # Handle the case of an empty --api=
+        --api=)             # Handle the case of an empty --api=
             die 'ERROR: "--api" requires a non-empty option argument.'
             ;;
-        -c|--critical)       # Takes an option argument; ensure it has been specified.
+        -c|--critical)      # Takes an option argument; ensure it has been specified.
             if [ "$2" ]; then
                 critical=$2
                 shift
             else
-                die 'ERROR: "--api" requires a non-empty option argument.'
+                die 'ERROR: "--critical" requires a non-empty option argument.'
             fi
             ;;
         --critical=?*)
-            critical=${1#*=} # Delete everything up to "=" and assign the remainder.
+            critical=${1#*=}# Delete everything up to "=" and assign the remainder.
             ;;
-        --critical=)         # Handle the case of an empty --critical=
+        --critical=)        # Handle the case of an empty --critical=
             die 'ERROR: "--critical" requires a non-empty option argument.'
+            ;;
+        -w|--warning)       # Takes an option argument; ensure it has been specified.
+            if [ "$2" ]; then
+                warning=$2
+                shift
+            else
+                die 'ERROR: "--warning" requires a non-empty option argument.'
+            fi
+            ;;
+        --warning=?*)
+            warning=${1#*=} # Delete everything up to "=" and assign the remainder.
+            ;;
+        --warning=)         # Handle the case of an empty --warning=
+            die 'ERROR: "--warning" requires a non-empty option argument.'
+            ;;
+        -t|--type)          # Takes an option argument; ensure it has been specified.
+            if [ "$2" ]; then
+                type=$2
+                shift
+            else
+                die 'ERROR: "--type" requires a non-empty option argument.'
+            fi
+            ;;
+        --type=?*)
+            type=${1#*=}    # Delete everything up to "=" and assign the remainder.
+            ;;
+        --type=)            # Handle the case of an empty --type=
+            die 'ERROR: "--type" requires a non-empty option argument.'
+            ;;
+        -q|--query)         # Takes an option argument; ensure it has been specified.
+            if [ "$2" ]; then
+                query=$2
+                shift
+            else
+                die 'ERROR: "--query" requires a non-empty option argument.'
+            fi 
+            ;;
+        --query=?*)
+            query=${1#*=}   # Delete everything up to "=" and assign the remainder.
+            ;;
+        --query=)           # Handle the case of an empty --query=
+            die 'ERROR: "--query" requires a non-empty option argument.'
             ;;
         -v|--verbose)
             verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
             ;;
-        --)              # End of all options.
+        --)                 # End of all options.
             shift
             break
             ;;
         -?*)
             printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
             ;;
-        *)               # Default case: No more options, so break out of the loop.
+        *)                  # Default case: No more options, so break out of the loop.
             break
     esac
 
@@ -64,7 +109,7 @@ while :; do
 done
 
 # if --api was provided
-if [ "$api" ] && [ "$critical" ]; then
+if [ "$api" ] && [ "$critical" ] && [ "$warning" ] && [ "$type" ] && [ "$query" ]; then
     export token=$(uaac context | grep access_token | awk '{print $2}')
-    curl -sG "$api" -H "Authorization: Bearer ${token}" | jq ".[] | select(.query|test(\"latency.uaa\")) | .threshold.critical = ${critical} | .threshold.type = \"UPPER\" | .threshold.warning = 20000" | curl -d @- -H "Authorization: Bearer ${token}" -H "Accept: application/json" -H "Content-Type: application/json" "$api"
+    curl -sG "$api" -H "Authorization: Bearer ${token}" | jq ".[] | select(.query|test(\"$query\")) | .threshold.critical = ${critical} | .threshold.type = \"${type}\" | .threshold.warning = ${warning}" | curl -d @- -H "Authorization: Bearer ${token}" -H "Accept: application/json" -H "Content-Type: application/json" "$api"
 fi
